ca27b7993d4ab893e4e3d24b5efa0ba9f5262fb7
Sabiya <sabiya.sachdeva@gmail.com>
2023-07-26 14:49:13 -0700

    first commit

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose=require("mongoose")
 const dbName = "Exams23002";
-const uri=`mongodb+srv://sabiya:123@exams23002cluster.szx2hab.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+//const uri=`mongodb+srv://sabiya:123@exams23002cluster.szx2hab.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+const uri=`mongodb+srv://tempuser:123@cluster0.f9d6o.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 mongoose.connect(uri,{
     useNewUrlParser:true,
     useUnifiedTopology: true,
